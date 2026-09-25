8659641187b3cd141fd2dc0e08f7dc724c116478
krishna1369 <100772377+krishna1369@users.noreply.github.com>
2026-09-25 07:19:28 +0530

    Add Terraform configuration for dev environment

---
+++ environment/dev/terraform.tf
@@ -0,0 +1,13 @@
+aws_region = "ap-south-1"
+
+ami_id = "ami-01a00762f46d584a1"
+
+instance_type = "c7i-flex.large"
+
+subnet_id = "subnet-06be8defeaf48d5c5"
+
+security_group_ids = [
+  "sg-0a10c6e5ce4469148"
+]
+
+key_name = "key"
